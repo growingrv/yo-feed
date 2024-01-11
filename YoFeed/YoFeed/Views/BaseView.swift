270c8376dee6058dd89e4f4ec695edf5208a8ce5
Gaurav <growingrv@gmail.com>
2024-01-11 18:38:38 +0530

    Add basic folder structure

---
+++ YoFeed/YoFeed/Views/BaseView.swift
@@ -0,0 +1,18 @@
+//
+//  BaseView.swift
+//  YoFeed
+//
+//  Created by Gaurav Tiwari on 2024-01-11.
+//
+
+import SwiftUI
+
+struct BaseView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    BaseView()
+}
